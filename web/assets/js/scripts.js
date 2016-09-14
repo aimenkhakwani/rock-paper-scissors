8498a8d872eac91f275d07ca1e158de1db82e8f1
Aimen Khakwani and Mark Lawson <aimen@>
2016-09-14 10:20:19 -0700

    add selection confirmation when user clicks a button

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,9 +1,11 @@
 $(function() {
     $('.btn_player1').click(function() {
         $('#input_player1').val($(this).text());
+        $('#hide1').fadeIn();
     });
 
     $('.btn_player2').click(function() {
         $('#input_player2').val($(this).text());
+        $('#hide2').fadeIn();
     });
 });
